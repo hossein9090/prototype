1ede96b27acb68d50195e30f1b71cfa8d715085c
hossein <hossein.525h.g@gmail.com>
2021-07-14 18:13:52 +0430

    prototypem

---
+++ prototype1.py
@@ -0,0 +1,49 @@
+import copy
+
+
+
+class Prototype:
+    def __init__(self):
+        self._objects = {}
+
+    def register_object(self, name, obj):
+        """Register an object"""
+        self._objects[name] = obj
+
+    def unregister_object(self, name):
+        """Unregister an object"""
+        del self._objects[name]
+
+    def clone(self, name, **attr):
+        """Clone a registered object and update its attributes"""
+        obj = copy.deepcopy(self._objects.get(name))
+        obj.__dict__.update(attr)
+        return obj
+
+
+
+class Car():
+    """Product"""
+    def __init__(self):
+        self.model = None
+        self.tires = None
+        self.engine = None
+
+    def __str__(self):
+        return '{} | {} | {}'.format(self.model, self.tires, self.engine)
+
+
+
+
+c = Car()
+c.model = "model"
+c.tires = "tire"
+c.engine = "engine"
+
+prototype = Prototype()
+prototype.register_object('model', c)
+
+c1 = prototype.clone('model')
+
+print(c1)
+
